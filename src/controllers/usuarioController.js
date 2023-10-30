7be49dcf8771ea0b0165f644e3a0134dd52dd604
ArthurSMA <arthursuassunamaiaalves@gmail.com>
2023-10-30 14:51:02 -0300

    master: adicionando o restante das opções crud ao controller

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -15,8 +15,61 @@ const buscarUsuarioPorID = (req, res) => {
         : res.status(404).json(notification.msg[4]);
 };
 
+const cadastrarUsuario = (req, res) => {
+    if (!req.body) return res.status(400).json(notification.msg[2]);
+
+    const novoUsuario = req.body;
+    const usuarioCadastrado = data.usuarios.find(
+        (usuario) =>
+            usuario.id === novoUsuario.id || usuario.nome === novoUsuario.nome
+    );
+
+    if (usuarioCadastrado) {
+        res.status(400).json(notification.msg[5]);
+    } else {
+        data.usuarios.push(novoUsuario);
+        res.status(201).json(novoUsuario);
+    }
+};
+
+const editarUsuario = (req, res) => {
+    if (!req.body) return res.status(400).json(notification.msg[2]);
+
+    const usuarioId = parseInt(req.params.id);
+    const novoUsuarioIndex = req.body;
+    const usuarioIndex = data.usuarios.findIndex(
+        (item) => item.id === usuarioId
+    );
+
+    if (usuarioIndex === -1) {
+        return res.status(404).json(notification.msg[4]);
+    }
+
+    data.usuarios[usuarioIndex] = {
+        ...data.usuarios[usuarioIndex],
+        ...novoUsuarioIndex,
+    };
+
+    res.json(data.usuarios[usuarioIndex]);
+};
+
+const deletarUsuario = (req, res) => {
+    const usuarioId = parseInt(req.params.id);
+    const usuario = data.usuarios.findIndex((item) => item.id === usuarioId);
+
+    if (usuario === -1) {
+        return res.status(404).json(notification.msg[4]);
+    }
+
+    data.usuarios.splice(usuario, 1);
+
+    res.status(204).send();
+};
 
 module.exports = {
     listarUsuarios,
-    buscarUsuarioPorID
+    buscarUsuarioPorID,
+    cadastrarUsuario,
+    editarUsuario,
+    deletarUsuario
 }
--- src/routes/usuarioRouter.js
+++ src/routes/usuarioRouter.js
@@ -4,5 +4,8 @@ const usuarioController = require('../controllers/usuarioController');
 
 router.get("/", usuarioController.listarUsuarios);
 router.get("/:id", usuarioController.buscarUsuarioPorID);
+router.post("/", usuarioController.cadastrarUsuario);
+router.put("/:id", usuarioController.editarUsuario);
+router.delete("/:id", usuarioController.deletarUsuario);
 
 module.exports = router;
